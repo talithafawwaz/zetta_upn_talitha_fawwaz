3639baafa5236e01f0c88dd7fdc87c987b2203c7
talithafawwaz <talithafawwaz07@gmail.com>
2023-03-08 23:02:05 +0700

    js_day_1

---
+++ index.js
@@ -0,0 +1,29 @@
+function bookPurchasing({title, price, category, author, status}, discount, tax) {
+    const amountOfDiscount = price * (discount / 100);
+    const priceAfterDiscount = price - amountOfDiscount;
+    const amountOfTax = priceAfterDiscount * (tax / 100);
+    const priceAfterTax = priceAfterDiscount + amountOfTax;
+    
+    return {
+        title: title,
+        price: price,
+        category: category,
+        author: author,
+        status : status,
+        amountOfDiscount: amountOfDiscount,
+        priceAfterDiscount: priceAfterDiscount,
+        amountOfTax: amountOfTax,
+        priceAfterTax: priceAfterTax
+    };
+}
+        
+let book = {
+    "title": "Hello Salma",
+    "price": 99000,
+    "category" : "Novel",
+    "author" : "Erisca Febriani",
+    "status" : true
+}
+
+
+console.log(bookPurchasing(book, 20, 5));
